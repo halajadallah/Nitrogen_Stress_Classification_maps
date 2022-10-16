732769e0825ecf1e65cb6ecd604152a78bcab484
halajadallah <htree2000@yahoo.com>
2022-10-16 08:22:59 +0300

    add pages

---
+++ Fertilizer_Recommendation.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+# This is the main streamlit app file
+
+st.set_page_config(page_title='Fertilzer Recommendation', layout = 'wide')
+
+
+st.title('Bangladesh Fertilzer Recommendation')
+
+st.header('Nitrogen Stress Detection Based On Satellite Data Analysis')
+
+st.markdown(
+'''
+## [Omdena Bangladesh Chapter](https://omdena.com/chapter-challenges/fertilizer-recommendation-system-to-ensure-high-crop-yields-in-bangladesh/)
+'''
+)
