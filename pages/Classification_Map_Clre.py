ed1046cb33a4a7c01372391cba8628da174aacce
halajadallah <htree2000@yahoo.com>
2022-10-16 14:43:57 +0300

    update map file

--- pages/Classification_Map_Clre.py
+++ pages/Classification_Map_Clre.py
@@ -4,23 +4,48 @@ import numpy as np
 import pickle
 import os
 
+#loading pickled geodata into numpy arrays 
 root_path = os.getcwd()
-boro_path = os.path.join(root_path, 'geodata', 'boro_clre_arrays.pkl')
-aman_path = os.path.join(root_path, 'geodata', 'aman_clre_arrays.pkl')
 bounds_path = os.path.join(root_path, 'geodata', 'center_bounds_list.pkl')
 
-pickle_boro_off = open (boro_path, "rb")
-boro_arrays_list = pickle.load(pickle_boro_off)
-
-pickle_aman_off = open (aman_path, "rb")
-aman_arrays_list = pickle.load(pickle_aman_off)
-
-boro_2018, boro_2019, boro_2020, boro_2021, boro_2022 = boro_arrays_list
-aman_2018, aman_2019, aman_2020, aman_2021 = aman_arrays_list
 
 pickle_bounds_off = open(bounds_path, "rb")
 bounds_list = pickle.load(pickle_bounds_off)
 centerx, centery, xmin, ymin, xmax, ymax = bounds_list
+
+def pickle_off(path):
+    pickle_off = open(path, "rb")
+    array = pickle.load(pickle_off)
+    return array
+
+
+boro_2018_ap = os.path.join(root_path, 'geodata', 'boro_2018_array.pkl')
+boro_2018 = pickle_off(boro_2018_ap)
+
+boro_2019_ap = os.path.join(root_path, 'geodata', 'boro_2019_array.pkl')
+boro_2019 = pickle_off(boro_2019_ap)
+
+boro_2020_ap = os.path.join(root_path, 'geodata', 'boro_2020_array.pkl')
+boro_2020 = pickle_off(boro_2020_ap)
+
+boro_2021_ap = os.path.join(root_path, 'geodata', 'boro_2021_array.pkl')
+boro_2021 = pickle_off(boro_2021_ap)
+
+boro_2022_ap = os.path.join(root_path, 'geodata', 'boro_2022_array.pkl')
+boro_2022 = pickle_off(boro_2022_ap)
+
+aman_2018_ap = os.path.join(root_path, 'geodata', 'aman_2018_array.pkl')
+aman_2018 = pickle_off(aman_2018_ap)
+
+aman_2019_ap = os.path.join(root_path, 'geodata', 'aman_2019_array.pkl')
+aman_2019 = pickle_off(aman_2019_ap)
+
+aman_2020_ap = os.path.join(root_path, 'geodata', 'aman_2020_array.pkl')
+aman_2020 = pickle_off(aman_2020_ap)
+
+aman_2021_ap = os.path.join(root_path, 'geodata', 'aman_2021_array.pkl')
+aman_2021 = pickle_off(aman_2021_ap)
+
 
 ## colors : R,G,B,alpha
 raster_to_coloridx = {
