dea90a418123db6a6bfd51a2a1abe8f344d562bb
halajadallah <htree2000@yahoo.com>
2022-10-21 22:27:48 +0300

    update 60

--- pages/60_Limitation.py
+++ pages/60_Limitation.py
@@ -4,8 +4,7 @@ st.title("Limitations")
 
 st.markdown("1. Time to complete this project.")
 
-st.markdown("2. Lack of input from on the ground farmers or assistants who can provide information about nitrogen related plant traits, 
-or nitrogen level in plants.")
+st.markdown("2. Lack of input from on the ground farmers or assistants who can provide information about nitrogen related plant traits, or nitrogen level in plants.")
 
 st.markdown("3. Heavy Clouds were an issue for clear GPS images of rainy seasons. In this case the Aus season (June To September).")
     
