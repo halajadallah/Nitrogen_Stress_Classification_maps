6d465d37eda05eb36bf0c5cc1dfbe5f80d72f737
halajadallah <htree2000@yahoo.com>
2022-10-21 22:42:32 +0300

    update 60

--- pages/60_Limitation.py
+++ pages/60_Limitation.py
@@ -13,13 +13,13 @@ st.markdown(
 '''
 1. Time to complete this project.
 
-2. Lack of input from on the ground farmers or assistants who can provide information about type od crop, growth stage, amount of nitrogen supplied 
+2. Lack of input from on the ground farmers or assistants who can provide information about type of crop, growth stage, amount of nitrogen supplied 
    to the soil and at what time, along with their field location.
 
 3. Lack of ground field data, such as type of crop, biomass, nitrogen concentration, leaf area index, and other nitrogen related plant
    traits, along with location. 
 
-4. Heavy clouds were for clear satellite images during rainy seasons. In particular the Aus season (June To August).
+4. Heavy clouds preclude the collection of clear satellite images during rainy seasons, in particular, the Aus season (June To August).
 
 '''
 )
