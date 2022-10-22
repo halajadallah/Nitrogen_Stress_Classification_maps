99e3737443a943873dcca39f205f7c5c19eaa22c
halajadallah <htree2000@yahoo.com>
2022-10-22 10:52:48 +0300

    added aman maps

--- pages/40_Results_Classification_Maps.py
+++ pages/40_Results_Classification_Maps.py
@@ -8,12 +8,6 @@ import folium
 st.title("Classification Maps")
 
 
-#root_path = os.getcwd()
-#boro_path = os.path.join(root_path, 'geodata', 'boro_clre_arrays.pkl')
-#aman_path = os.path.join(root_path, 'geodata', 'aman_clre_arrays.pkl')
-#bounds_path = os.path.join(root_path, 'geodata', 'center_bounds_list.pkl')
-
-
 def pickle_off(path):
     pickle_off = open(path, "rb")
     array = pickle.load(pickle_off)
@@ -28,10 +22,10 @@ raster_to_coloridx = {
  2: (0.8941176470588236, 0.10196078431372549, 0.10980392156862745,0.7), #red
  3: (1.0, 1.0, 0.2, 0.7), #yellow
  4: (0.1, 0.6, 0.1, 0.7), #green 
- 5: (0.596078431372549, 0.3058823529411765, 0.6392156862745098, 0.7), #purple
+ 5: (23.0/255, 35.0/255, 246.0/255, 0.7), #purple
  -128: (1, 1, 1, 0)}
 
-m = folium.Map(location=[centery, centerx], zoom_start=11 ,tiles='openstreetmap')#'Stamen Terrain')
+m = folium.Map(location=[centery, centerx], zoom_start=13 ,tiles='openstreetmap')#'Stamen Terrain')
 st.write('Classes : Poor (-2 to -1) (red), Mild Stree (-1 to 0) (yellow), Normal (0 to 1) (green), Good (1 to 2) (purple)')
 
 folium.raster_layers.ImageOverlay(
@@ -89,6 +83,53 @@ folium.raster_layers.ImageOverlay(
     zindex=1,
     ).add_to(m)
 
+
+folium.raster_layers.ImageOverlay(
+    name="aman 2018",
+    image=aman_2018,
+    bounds=[[ymin, xmin], [ymax, xmax]],
+    opacity=0.7,
+    colormap=lambda x: raster_to_coloridx[x],
+    interactive=True,
+    cross_origin=False,
+    zindex=1,
+    ).add_to(m)
+    
+   
+folium.raster_layers.ImageOverlay(
+    name="aman 2019",
+    image=aman_2019,
+    bounds=[[ymin, xmin], [ymax, xmax]],
+    opacity=0.7,
+    colormap=lambda x: raster_to_coloridx[x],
+    interactive=True,
+    cross_origin=False,
+    zindex=1,
+    ).add_to(m)
+    
+folium.raster_layers.ImageOverlay(
+    name="aman 2020",
+    image=aman_2020,
+    bounds=[[ymin, xmin], [ymax, xmax]],
+    opacity=0.7,
+    colormap=lambda x: raster_to_coloridx[x],
+    interactive=True,
+    cross_origin=False,
+    zindex=1,
+    ).add_to(m)
+    
+folium.raster_layers.ImageOverlay(
+    name="aman 2021",
+    image=aman_2021,
+    bounds=[[ymin, xmin], [ymax, xmax]],
+    opacity=0.7,
+    colormap=lambda x: raster_to_coloridx[x],
+    interactive=True,
+    cross_origin=False,
+    zindex=1,
+    ).add_to(m)
+    
+# add pin to select/ unselect image        
 folium.LayerControl().add_to(m)
 
 m
