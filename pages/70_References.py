c2dfcb7b835e3cb9e1eae541d865554f37a5abd7
halajadallah <htree2000@yahoo.com>
2022-10-22 14:58:22 +0300

    references added

--- pages/70_References.py
+++ pages/70_References.py
@@ -1,3 +1,24 @@
 import streamlit as st
 
 st.title("References")
+
+st.markdown(
+'''
+* [Singha, M., Dong, J., Zhang, G. et al. High resolution paddy rice maps in cloud-prone Bangladesh and 
+  Northeast India using Sentinel-1 data. Sci Data 6, 26 (2019).](https://doi.org/10.1038/s41597-019-0036-3)
+
+* [Brinkhoff, J.; Dunn, B, ;Robson, A. Rice nitrogen status detection using commercial-scale imagery, International Journal of Applied Earth Observation and 
+  Geoinformation, Volume 105, 2021.](https://doi.org/10.1016/j.jag.2021.102627)
+
+* [Iatrou, M.; Karydas, C.; Iatrou, G.; Pitsiorlas, I.; Aschonitis, V.; Raptis, I.; Mpetas, S.; Kravvas, K.; Mourelatos, S. Topdressing Nitrogen Demand Prediction in Rice Crop Using 
+  Machine Learning Systems. Agriculture 2021, 11, 312.](https://doi.org/10.3390/agriculture11040312)
+  
+* [Perich, G.; Aasen, H.; Verrelst, J.; Argento, F.;Walter, A.; Liebisch, F. Crop Nitrogen Retrieval Methods for Simulated
+  Sentinel-2 Data Using In-Field Spectrometer Data. Remote Sens. 2021, 13, 2404.](https://doi.org/10.3390/rs13122404)  
+  
+* [Ariel Zajdband, Monitoring Crop Nitrogen Status with Satellite Data](https://medium.com/planet-stories/monitoring-crop-nitrogen-status-with-satellite-data-f906d1123f1a)
+ 
+* [Vegetation Indices for Chlorophyll (CI – MTCI – NDRE – ND705 – ND550 – mNDblue)](https://www.hiphen-plant.com/vegetation-indices-chlorophyll/3612/)
+
+'''
+)
